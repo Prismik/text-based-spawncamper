58baa77fec538423bcaa7b8373dc7a17b26f56b8
wbros <william.brossard@gmail.com>
2015-05-24 15:04:16 -0400

    Classes restructuration

---
+++ server/tile.py
@@ -0,0 +1,13 @@
+class Tile:
+  def __init__ (self):
+    self.messages = []
+    self.content = []
+    self.observable = True
+
+  def isObservable(self):
+    if self.observable
+      return False
+
+    for object in self.content:
+      if not self.content.isObservable()
+        return False
